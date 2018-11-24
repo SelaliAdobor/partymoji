ce312698a0decf8a227af7a20d680943f566272f
Selali Adobor <selali.adobor@willowtreeapps.com>
2018-11-24 12:51:10 -0500

    Wait for output stream to close when creating gifs

--- partify.js
+++ partify.js
@@ -1,9 +1,16 @@
 const fs = require("fs");
 const PartyPartyParty = require("party-party-party");
 
-export function partify(inPath, outPath){
-    return new Promise(()=>{
+export function partify(inPath, outPath) {
+    return new Promise((resolve) => {
         let outputFileStream = fs.createWriteStream(outPath);
-        PartyPartyParty(inPath, outputFileStream, 10);
+        
+        outputFileStream.on('finish', () => {
+            resolve()
+        });
+
+        PartyPartyParty(inPath, outputFileStream, 10, undefined, () => {
+            outputFileStream.end()
+        });
     })
 }
